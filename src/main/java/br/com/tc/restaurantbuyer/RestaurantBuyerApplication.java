3f07a4ee139f03c107c5e5cf95ff5a9011be35ba
maihe <maiaraoliveira.dejesus@gmail.com>
2023-10-04 11:44:15 -0300

    Initial Commit

---
+++ src/main/java/br/com/tc/restaurantbuyer/RestaurantBuyerApplication.java
@@ -0,0 +1,15 @@
+package br.com.tc.restaurantbuyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class RestaurantBuyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBuyerApplication.class, args);
+		System.out.println(" ***** Hello Time FIAP *****");
+	}
+
+}
---
+++ src/test/java/br/com/tc/restaurantbuyer/RestaurantBuyerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tc.restaurantbuyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBuyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
